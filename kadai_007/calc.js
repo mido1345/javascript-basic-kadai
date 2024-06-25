67d67f13a6c5694602020333ed9f1a00c105677b
mido1345 <127962412+mido1345@users.noreply.github.com>
2024-06-25 16:08:41 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,18 @@
+for (let num = 1; num <= 100; num++) {
+  
+  if (num % 3 === 0) {
+    console.log("3の倍数です");
+  }
+
+  if (num % 5 === 0) {
+      console.log("5の倍数です");
+  }
+
+  if(num % 3 === 0 && num % 5 === 0){
+    console.log("3と5の倍数です");
+}
+
+  else {
+      console.log(num);
+    }
+}
